48a8fdcf41606c80a879ee31405d19f02e45e871
Titan648 <142923960+Titan648@users.noreply.github.com>
2025-11-11 07:39:52 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.https://tgrgiiflocfxodfxdvtv.supabase.co;
-const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncmdpaWZsb2NmeG9kZnhkdnR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MjUzNTksImV4cCI6MjA3ODQwMTM1OX0.zVjpeB8CjDxhWMSLF27IX5NwhkEMREgMeiMEtrkRmx8;
+const supabaseUrl = 'https://tgrgiiflocfxodfxdvtv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncmdpaWZsb2NmeG9kZnhkdnR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MjUzNTksImV4cCI6MjA3ODQwMTM1OX0.zVjpeB8CjDxhWMSLF27IX5NwhkEMREgMeiMEtrkRmx8';
 
-export const supabase = createClient(https://tgrgiiflocfxodfxdvtv.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncmdpaWZsb2NmeG9kZnhkdnR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MjUzNTksImV4cCI6MjA3ODQwMTM1OX0.zVjpeB8CjDxhWMSLF27IX5NwhkEMREgMeiMEtrkRmx8);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);

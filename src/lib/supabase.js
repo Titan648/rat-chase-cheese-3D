a8b6282104efce2fdcaaebdef8fef84727be8442
Titan648 <142923960+Titan648@users.noreply.github.com>
2025-11-11 14:53:13 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,7 @@
-M
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://tgrgiiflocfxodfxdvtv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncmdpaWZsb2NmeG9kZnhkdnR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MjUzNTksImV4cCI6MjA3ODQwMTM1OX0.zVjpeB8CjDxhWMSLF27IX5NwhkEMREgMeiMEtrkRmx8';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
